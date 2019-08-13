ce150d17812a6111526333a925a1b8b5bb3cc79c
dylan-t-joyce <dylantjoyce@gmail.com>
2019-08-13 19:36:09 -0400

    changed test2

--- test2.py
+++ test2.py
@@ -1,4 +1,12 @@
-promise = "I Love You"
+list_b = list(range(10))
+print(list(list_b))
 
-for i in range(5):
-    print (promise)
+list_b_squared = []
+
+for i in list_b:
+    i_squared = i**2
+    list_b_squared.append(i_squared)
+    print(list_b_squared)
+    
+
+#python test2.py
