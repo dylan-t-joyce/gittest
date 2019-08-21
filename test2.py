b5f66fd1dad1877a9177a2281aed4c1e828a0258
dylan-t-joyce <dylantjoyce@gmail.com>
2019-08-20 21:29:40 -0400

    add names to greeting

--- test2.py
+++ test2.py
@@ -1,12 +1,10 @@
-list_b = list(range(10))
-print(list(list_b))
+def add_greetings(names):
+  list_of_names = []
+  for name in names:
+    list_of_names.append("Hello, %s" % name)
+  return list_of_names
 
-list_b_squared = []
-
-for i in list_b:
-    i_squared = i**2
-    list_b_squared.append(i_squared)
-    print(list_b_squared)
+print(add_greetings(["Owen", "Max", "Sophie"]))
     
 
 #python test2.py
